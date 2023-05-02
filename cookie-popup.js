d9b6598d206aade8e7197edc7986ea5b4d43c822
Charlie Rolecek <csrolecek@uiowa.edu>
2023-05-01 20:23:23 -0500

    updating cookie popup

--- cookie-popup.js
+++ cookie-popup.js
@@ -1,50 +1,25 @@
-// Get the cookie popup element
-const cookiePopup = document.getElementById("cookie-popup");
+// Get the cookie consent popup element
+var cookiePopup = document.querySelector('.cookie-popup');
 
-// Get the accept button
-const acceptCookies = document.getElementById("accept-cookies");
+// Get the accept and decline buttons
+var acceptBtn = document.querySelector('#accept-cookies');
+var declineBtn = document.querySelector('#decline-cookies');
 
-// Function to set a cookie
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  const expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// Check if the user has already accepted cookies
+var cookiesAccepted = (document.cookie.indexOf('cookiesAccepted=true') !== -1);
+
+// If cookies are not accepted, show the cookie popup
+if (!cookiesAccepted) {
+	cookiePopup.style.display = 'block';
 }
 
-// Function to check if the cookie has been set
-function checkCookie() {
-  const cookieAccepted = getCookie("cookieAccepted");
-  if (cookieAccepted === "") {
-    // Show the cookie popup
-    cookiePopup.style.display = "block";
-  }
-}
-
-// Function to get a cookie value
-function getCookie(cname) {
-  const name = cname + "=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-// Event listener for the accept button
-acceptCookies.addEventListener("click", function () {
-  // Set the cookie
-  setCookie("cookieAccepted", "true", 30);
-  // Hide the cookie popup
-  cookiePopup.style.display = "none";
+// When the user clicks the accept button, set a cookie and hide the popup
+acceptBtn.addEventListener('click', function() {
+	document.cookie = 'cookiesAccepted=true; expires=' + new Date(new Date().getTime() + (365 * 24 * 60 * 60 * 1000)).toGMTString() + '; path=/';
+	cookiePopup.style.display = 'none';
 });
 
-// Call the checkCookie function
-check
+// When the user clicks the decline button, hide the popup
+declineBtn.addEventListener('click', function() {
+	cookiePopup.style.display = 'none';
+});
